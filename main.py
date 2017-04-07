53030d08f0637d5e61f561e19e04392b717a3f20
Timothée <timothee.aupetit@gmail.com>
2017-04-07 23:29:54 +0200

    created main.py to isolate main programme

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 7  22:30:07 2017
+
+@author: timotheeaupetit
+"""
+
+from weather_charts import WeatherCharts
+
+def main():
+    m = WeatherCharts()
+    m.load_cities()
+    m.select_graph()
+    
+if __name__ == '__main__':
+    main()
+    
