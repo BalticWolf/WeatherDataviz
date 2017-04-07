8e03e551644a04b479bb0b2e350f3f9d59043cee
Timothée <timothee.aupetit@gmail.com>
2017-04-07 23:34:21 +0200

    created city dependencies (new classes)

--- citylist.py
+++ citylist.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 25 09:05:31 2016
+Created on Fri Nov 25 09:05 2016
+Updated on Fri Apr 07 23:07 2017
 
 @author: timotheeaupetit
 """
-# this must come first
-from __future__ import print_function
 
     
 class CityList(list):
@@ -15,3 +14,21 @@ class CityList(list):
     
     def add_city(self, city):
         self.append(city)
+    
+class City(object):
+    def __init__(self, json_data):
+        self.name = json_data['city']['name']
+        self.longitude = json_data['city']['coord']['lon']
+        self.latitude = json_data['city']['coord']['lat']
+        self.data = Measure(json_data['data']) # should be a list of measures
+
+class Measure(object):
+    def __init__(self, json_data):
+        self.clouds = json_data['clouds']
+        # self.deg = json_data['deg'] # can't recall what this is
+        self.date = json_data['dt']
+        self.humidity = json_data['humidity']
+        self.pressure = json_data['pressure']
+        self.wind_speed = json_data['speed']
+        self.temperatures = json_data['temp'] # should be a list
+        self.weather = json_data['weather'] # should be a list
