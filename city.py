e5cc8231b8191dfa41670cfff4757b8bf53be1b9
Timothée <timothee.aupetit@gmail.com>
2017-04-08 09:04:24 +0200

    added some comments in the code

--- city.py
+++ city.py
@@ -11,38 +11,49 @@ import time
     
 class City(object):
     def __init__(self, json_data):
+        # self.id = json_data['city']['id'] # Not used
         self.name = json_data['city']['name']
+        #self.country = json_data['city']['country'] # Not used
+        
         self.longitude = json_data['city']['coord']['lon']
         self.latitude = json_data['city']['coord']['lat']
+        
         self.data = self.list_measures(json_data['data'])
     
     def list_measures(self, json_data):
-        self.measures = []
+        measures = []
         for measure in json_data:
             self.measures.append(Measure(measure))
         
-class Measure(object):
-    def __init__(self, json_data):
-        self.clouds = json_data['clouds']
-        # self.deg = json_data['deg'] # can't recall what this is
-        self.date = Measure.format_date(json_data['dt'])
-        self.humidity = json_data['humidity']
-        self.pressure = json_data['pressure']
-        #self.speed = json_data['speed']
-
-        self.t_morn = Measure.kelvin_to_celcius(json_data['temp']['morn'])
-        self.t_day = Measure.kelvin_to_celcius(json_data['temp']['day'])
-        self.t_eve = Measure.kelvin_to_celcius(json_data['temp']['eve'])
-        self.t_night = Measure.kelvin_to_celcius(json_data['temp']['night'])
+        return measures
         
-        # self.weather = json_data['weather']
+class Measure(object):
+    """
+    A Measure contain weather data taken at a certain time ('dt')
+    """
+    def __init__(self, json_data):
+        # self.clouds = json_data['clouds']  # Not used
+        # self.deg = json_data['deg']  # Not used
+        self.date = Measure.format_date(json_data['dt'])
+        # self.humidity = json_data['humidity']  # Not used
+        self.pressure = json_data['pressure']
+        # self.speed = json_data['speed'] # Not used
+
+        self.t_morn =   Measure.kelvin_to_celcius(json_data['temp']['morn'])
+        self.t_day =    Measure.kelvin_to_celcius(json_data['temp']['day'])
+        self.t_eve =    Measure.kelvin_to_celcius(json_data['temp']['eve'])
+        self.t_night =  Measure.kelvin_to_celcius(json_data['temp']['night'])
+        
+        # self.weather = json_data['weather'] # Not used
 
     def format_date(dt):
-        # *Y*ear *m*onth *d*ay *H*our *M*inute
-        date_format="%Y-%m-%d:%H-%M" # UTC
+        """
+        This method converts a date (given in seconds since the Epoch) 
+        in the format *Y*ear *m*onth *d*ay *H*our *M*inute
+        """
+        pattern="%Y-%m-%d:%H-%M" # UTC
         
-        # gmtime returns UTC hour (formerly GMT)
-        return time.strftime(date_format, time.gmtime(dt))
+        return time.strftime(pattern, time.gmtime(dt))
     
     def kelvin_to_celcius(k_temp):
         """
